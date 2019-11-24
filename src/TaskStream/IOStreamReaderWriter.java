8b067753f77908aa2e1bf6864a701ab824007635
avanghelovici <artur.vanghelovici@endava.com>
2019-11-24 17:27:59 +0200

    stream task

---
+++ src/TaskStream/IOStreamReaderWriter.java
@@ -0,0 +1,34 @@
+package TaskStream;
+
+
+import java.io.*;
+
+public class IOStreamReaderWriter {
+
+   private static InputStreamReader reader;
+   private static FileInputStream fileInputStream;
+
+
+    public static void read(File test){
+        try {
+            fileInputStream = new FileInputStream(test);
+            reader = new InputStreamReader(fileInputStream);
+            {
+                int character;
+                while ((character = reader.read()) != -1) {
+                    System.out.print((char) character);
+                }
+                System.out.print("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void write(String st, File test) throws IOException {
+        try (FileOutputStream fileStream = new FileOutputStream(test);
+             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileStream)) {
+            outputStreamWriter.write(st);
+        }
+    }
+}
---
+++ src/TaskStream/TestData.java
@@ -0,0 +1,17 @@
+package TaskStream;
+
+
+import java.io.File;
+
+public class TestData {
+    public static File testInputStream = new File("C:\\Users\\avanghelovici\\IdeaProjects\\test1.txt");
+    public static File testOutputStream = new File("C:\\Users\\avanghelovici\\IdeaProjects\\test2.txt");
+    public static File testReader = new File("C:\\Users\\avanghelovici\\IdeaProjects\\test3.txt");
+    public static File testWriter = new File("C:\\Users\\avanghelovici\\IdeaProjects\\test4.txt");
+    public static File iOreaderTest = new File("C:\\Users\\avanghelovici\\IdeaProjects\\test5.txt");
+    public static File iOwriterTest = new File("C:\\Users\\avanghelovici\\IdeaProjects\\test6.txt");
+    public static File bufferedITest = new File("C:\\Users\\avanghelovici\\IdeaProjects\\test7.txt");
+    public static File bufferedOTest = new File("C:\\Users\\avanghelovici\\IdeaProjects\\test8.txt");
+    public static File fileReaderTest = new File("C:\\Users\\avanghelovici\\IdeaProjects\\test9.txt");
+    public static File fileWriterTest = new File("C:\\Users\\avanghelovici\\IdeaProjects\\test10.txt");
+}
