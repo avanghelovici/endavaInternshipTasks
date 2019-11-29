28796eff6b886722574bf7ad17b79e14848c5dc5
avanghelovici <artur.vanghelovici@endava.com>
2019-11-29 17:38:00 +0200

    junit

--- JunitTask/src/test/java/test/junit/TestMethods.java
+++ JunitTask/src/test/java/test/junit/TestMethods.java
@@ -1,21 +1,21 @@
-package JunitTask;
+package TestJunit;
 
 import org.hamcrest.Matcher;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.testng.Assert;
 
 import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.is;
 
-class Tests {
+class TestMethods {
     private static long startTime;
 
     @BeforeAll
@@ -37,7 +37,7 @@ class Tests {
     @DisplayName("Multiply test using CsvSource(Positive)")
     @ParameterizedTest(name = "Multiply between {0} and {1}")
     void multiply1(Double firstNumber, Double secondNumber) {
-        final TestData test = new TestData(firstNumber, secondNumber, "multiply");
+        final Methods test = new Methods(firstNumber, secondNumber, "multiply");
         final Double actualResult = test.calculate();
         final Double expectedResult = (test.getFirstDoubleNumber() * test.getSecondDoubleNumber());
 
@@ -48,7 +48,7 @@ class Tests {
     @DisplayName("Multiply test using CsvSource(Negative)")
     @ParameterizedTest(name = "Multiply between {0} and {1}")
     void multiply2(Double firstNumber, Double secondNumber) {
-        final TestData test = new TestData(firstNumber, secondNumber, "multiply");
+        final Methods test = new Methods(firstNumber, secondNumber, "multiply");
         final Double actualResult = test.calculate();
         final Double expectedResult = (test.getFirstDoubleNumber() * test.getSecondDoubleNumber());
 
@@ -59,7 +59,7 @@ class Tests {
     @DisplayName("Multiply test using CsvSource(Using 0)")
     @ParameterizedTest(name = "Multiply between {0} and {1}")
     void multiply3(Double firstNumber, Double secondNumber) {
-        final TestData test = new TestData(firstNumber, secondNumber, "multiply");
+        final Methods test = new Methods(firstNumber, secondNumber, "multiply");
         final Double actualResult = test.calculate();
         final Double expectedResult = (test.getFirstDoubleNumber() * test.getSecondDoubleNumber());
 
@@ -70,7 +70,7 @@ class Tests {
     @DisplayName("Divide test using CsvSource(Positive)")
     @ParameterizedTest(name = "Multiply between {0} and {1}")
     void divide1(Double firstNumber, Double secondNumber) {
-        final TestData test = new TestData(firstNumber, secondNumber, "divide");
+        final Methods test = new Methods(firstNumber, secondNumber, "divide");
         final Double actualResult = test.calculate();
         final Double expectedResult = (test.getFirstDoubleNumber() / test.getSecondDoubleNumber());
 
@@ -81,51 +81,47 @@ class Tests {
     @DisplayName("Divide test using CsvSource(Negative)")
     @ParameterizedTest(name = "Multiply between {0} and {1}")
     void divide2(Double firstNumber, Double secondNumber) {
-        final TestData test = new TestData(firstNumber, secondNumber, "divide");
+        final Methods test = new Methods(firstNumber, secondNumber, "divide");
         final Double actualResult = test.calculate();
         final Double expectedResult = (test.getFirstDoubleNumber() / test.getSecondDoubleNumber());
 
         MatcherAssert.assertThat("actual result matches expected result", !actualResult.equals(expectedResult));
     }
 
-    @CsvSource(value = {"12.0,8.0", "22.0, 4.0", "25.0, 6.50"})
+    @CsvSource(value = {"12,8", "22, 4", "21, 6"})
     @DisplayName("Subtract test using CsvSource(Positive)")
-    @ParameterizedTest(name = "Multiply between {0} and {1}")
-    void subtract1(Double firstNumber, Double secondNumber) {
-        final TestData test = new TestData(firstNumber, secondNumber, "subtract");
+    @ParameterizedTest(name = "Subtract between {0} and {1}")
+    void subtract1(Integer firstNumber, Integer secondNumber) {
+        final Methods test = new Methods(firstNumber, secondNumber, "subtract");
         final Double actualResult = test.calculate();
-        final Double expectedResult = (test.getFirstDoubleNumber() - test.getSecondDoubleNumber());
-
+        final Double expectedResult = (double) test.getFirstNumber() - test.getSecondNumber();
         MatcherAssert.assertThat("actual result matches expected result", actualResult, is(expectedResult));
     }
 
     @CsvSource(value = {"2.0,8.0", "-2.0, 4.0", "25.0, -6.50"})
     @DisplayName("Subtract test using CsvSource(Negative)")
-    @ParameterizedTest(name = "Multiply between {0} and {1}")
+    @ParameterizedTest(name = "Subtract between {0} and {1}")
     void subtract2(Double firstNumber, Double secondNumber) {
-        final TestData test = new TestData(firstNumber, secondNumber, "subtract");
+        final Methods test = new Methods(firstNumber, secondNumber, "subtract");
         final Double actualResult = test.calculate();
-        final Double expectedResult = (test.getFirstDoubleNumber() - test.getSecondDoubleNumber());
+        final Double expectedResult = (double) (test.getFirstNumber() - test.getSecondNumber());
 
         MatcherAssert.assertThat("actual result matches expected result", !actualResult.equals(expectedResult));
     }
 
-    @CsvSource(value = {"0,2.22", "2, 0", "-1.22,0"})
+    @CsvSource(value = {"2,0", "5,0", "15,0"})
     @DisplayName("Divide test using CsvSource(Negative)")
     @ParameterizedTest(name = "divide between {0} and {1}")
     void exceptionThrowsTest(Double firstNumber, Double secondNumber) {
-        final TestData test = new TestData(firstNumber, secondNumber, "divide");
-        final Double actualResult = test.calculate();
-        final Double expectedResult = (test.getFirstDoubleNumber() / test.getSecondDoubleNumber());
-        MatcherAssert.assertThat("actual result matches expected result", !actualResult.equals(expectedResult));
-        Assert.assertThrows(NullPointerException.class, ()-> test.calculate());
+        final Methods test = new Methods(firstNumber, secondNumber, "divide");
+        Assertions.assertThrows(ArithmeticException.class, test::calculate);
     }
 
     @MethodSource("TestMultiply")
     @DisplayName("Test using MethodSource multiply (Positive)")
     @ParameterizedTest(name = "{2} test on values {0} and {1}")
     void methodsUsing1(Double firstNumber, Double secondNumber, String method, Matcher<Double> matcher) {
-        final TestData test = new TestData(firstNumber, secondNumber, method);
+        final Methods test = new Methods(firstNumber, secondNumber, method);
         final Double actualResult = test.calculate();
         MatcherAssert.assertThat(actualResult, matcher);
     }
@@ -142,7 +138,7 @@ class Tests {
     @DisplayName("Test using MethodSource multiply (Negative)")
     @ParameterizedTest(name = "{2} tests on values {0} and {1}")
     void methodsUsing2(Double firstNumber, Double secondNumber, String method, Matcher<Double> matcher) {
-        final TestData test = new TestData(firstNumber, secondNumber, method);
+        final Methods test = new Methods(firstNumber, secondNumber, method);
         final Double actualResult = test.calculate();
         MatcherAssert.assertThat(actualResult, matcher);
     }
@@ -159,7 +155,7 @@ class Tests {
     @DisplayName("Test using MethodSource divide (Positive)")
     @ParameterizedTest(name = "{2} tests on values {0} and {1}")
     void methodsUsing3(Double firstNumber, Double secondNumber, String method, Matcher<Double> matcher) {
-        final TestData test = new TestData(firstNumber, secondNumber, method);
+        final Methods test = new Methods(firstNumber, secondNumber, method);
         final Double actualResult = test.calculate();
         MatcherAssert.assertThat(actualResult, matcher);
     }
@@ -176,7 +172,7 @@ class Tests {
     @DisplayName("Test using MethodSource divide (Negative)")
     @ParameterizedTest(name = "{2} tests on values {0} and {1}")
     void methodsUsing4(Double firstNumber, Double secondNumber, String method, Matcher<Double> matcher) {
-        final TestData test = new TestData(firstNumber, secondNumber, method);
+        final Methods test = new Methods(firstNumber, secondNumber, method);
         final Double actualResult = test.calculate();
         MatcherAssert.assertThat(actualResult, matcher);
     }
@@ -193,7 +189,7 @@ class Tests {
     @DisplayName("Test using MethodSource subtract (Negative)")
     @ParameterizedTest(name = "{2} tests on values {0} and {1}")
     void methodsUsing5(Integer firstNumber, Integer secondNumber, String method, Matcher<Double> matcher) {
-        final TestData test = new TestData(firstNumber, secondNumber, method);
+        final Methods test = new Methods(firstNumber, secondNumber, method);
         final Double actualResult = test.calculate();
         MatcherAssert.assertThat(actualResult, matcher);
     }
@@ -210,7 +206,7 @@ class Tests {
     @DisplayName("Test using MethodSource subtract (Positive)")
     @ParameterizedTest(name = "{2} tests on values {0} and {1}")
     void methodsUsing6(Integer firstNumber, Integer secondNumber, String method, Matcher<Double> matcher) {
-        final TestData test = new TestData(firstNumber, secondNumber, method);
+        final Methods test = new Methods(firstNumber, secondNumber, method);
         final Double actualResult = test.calculate();
         MatcherAssert.assertThat(actualResult, matcher);
     }
