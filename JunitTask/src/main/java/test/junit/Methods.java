8a0ad41e31d2a846b65ac04880894ba102a625e6
avanghelovici <artur.vanghelovici@endava.com>
2019-11-29 17:38:00 +0200

    junit

--- JunitTask/src/main/java/test/junit/Methods.java
+++ JunitTask/src/main/java/test/junit/Methods.java
@@ -1,12 +1,21 @@
-package JunitTask;
+package test.junit;
 
-import org.testng.Assert;
-
-class TestData {
+class Methods {
 
     private double firstDoubleNumber;
     private double secondDoubleNumber;
+    private int firstNumber;
+    private int secondNumber;
     private String method;
+
+
+    int getFirstNumber() {
+        return firstNumber;
+    }
+
+    int getSecondNumber() {
+        return secondNumber;
+    }
 
     double getFirstDoubleNumber() {
         return firstDoubleNumber;
@@ -20,32 +29,42 @@ class TestData {
         return method;
     }
 
-    TestData(double firstDoubleNumber, double secondDoubleNumber, String method) {
+    Methods(int firstNumber, int secondNumber, String method) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+        this.method = method;
+    }
+
+    Methods(double firstDoubleNumber, double secondDoubleNumber, String method) {
         this.firstDoubleNumber = firstDoubleNumber;
         this.secondDoubleNumber = secondDoubleNumber;
         this.method = method;
     }
 
-    public double multiply(double firstNumber, double secondNumber) {
+    private double multiply(double firstNumber, double secondNumber) {
         if ((firstNumber >= 0 && firstNumber <= 100) && (secondNumber >= 0 && secondNumber <= 100)) {
             return firstNumber * secondNumber;
         } else return 0;
     }
 
-    private double subtract(double firstNumber, double secondNumber) {
-        if ((firstNumber>=0&&secondNumber>=0)&&(firstNumber > secondNumber)) {
+    private int subtract(int firstNumber, int secondNumber) {
+        if ((firstNumber >= 0 && secondNumber >= 0) && (firstNumber > secondNumber)) {
             return firstNumber - secondNumber;
         } else {
             return -1;
         }
     }
 
-    private double divide(double firstDoubleNumber, double secondDoubleNumber) {
-        if ((firstDoubleNumber > 0 && secondDoubleNumber > 0) && (firstDoubleNumber >= secondDoubleNumber)) {
-            return firstDoubleNumber / secondDoubleNumber;}
-        else
-        return 0;
+    private double divide(double firstNumber, double secondNumber) {
+        if ((firstNumber >= 0 && secondNumber >= 0) && firstNumber >= secondNumber) {
+            if (firstNumber / secondNumber == Double.POSITIVE_INFINITY) {
+                throw new ArithmeticException("divide by zero");
+            } else {
+                return firstNumber / secondNumber;
+            }
+        } else return 0;
     }
+
     Double calculate() {
         Double result = null;
         switch (getMethod()) {
@@ -56,7 +75,7 @@ class TestData {
                 result = divide(getFirstDoubleNumber(), getSecondDoubleNumber());
                 break;
             case "subtract":
-                result = subtract(getFirstDoubleNumber(), getSecondDoubleNumber());
+                result = (double) subtract(getFirstNumber(), getSecondNumber());
                 break;
             default:
                 System.out.println("No such method");
@@ -64,5 +83,7 @@ class TestData {
         }
         return result;
     }
+
+
 }
 
