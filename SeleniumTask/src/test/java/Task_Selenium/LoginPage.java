fa0d095e2ebfce9261e344183a516a9546232974
avanghelovici <artur.vanghelovici@endava.com>
2019-12-02 20:47:28 +0200

    Selenium final+-

--- SeleniumTask/src/test/java/Task_Selenium/LoginPage.java
+++ SeleniumTask/src/test/java/Task_Selenium/LoginPage.java
@@ -1,4 +1,26 @@
 package Task_Selenium;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage {
+
+    @FindBy(xpath = "//div[2]/div/form/input")
+    private WebElement loginButton;
+    @FindBy(xpath = "//div/div[2]/div/form/div[1]/input")
+    private WebElement email;
+    @FindBy(xpath = "//div/div/div[2]/div/form/div[2]/input")
+    private WebElement pass;
+
+    WebElement getPass() {
+        return pass;
+    }
+
+    WebElement getEmail() {
+        return email;
+    }
+
+    WebElement getLoginButton() {
+        return loginButton;
+    }
 }
