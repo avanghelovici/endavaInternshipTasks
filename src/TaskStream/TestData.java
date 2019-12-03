b234d090c85c4376b318079b292f366cc3461f51
avanghelovici <artur.vanghelovici@endava.com>
2019-12-03 21:22:39 +0200

    Stream last commit

--- src/TaskStream/TestData.java
+++ src/TaskStream/TestData.java
@@ -4,14 +4,14 @@ package TaskStream;
 import java.io.File;
 
 public class TestData {
-    public static File testInputStream = new File("C:\\Users\\avanghelovici\\IdeaProjects\\test1.txt");
-    public static File testOutputStream = new File("C:\\Users\\avanghelovici\\IdeaProjects\\test2.txt");
-    public static File testReader = new File("C:\\Users\\avanghelovici\\IdeaProjects\\test3.txt");
-    public static File testWriter = new File("C:\\Users\\avanghelovici\\IdeaProjects\\test4.txt");
-    public static File iOreaderTest = new File("C:\\Users\\avanghelovici\\IdeaProjects\\test5.txt");
-    public static File iOwriterTest = new File("C:\\Users\\avanghelovici\\IdeaProjects\\test6.txt");
-    public static File bufferedITest = new File("C:\\Users\\avanghelovici\\IdeaProjects\\test7.txt");
-    public static File bufferedOTest = new File("C:\\Users\\avanghelovici\\IdeaProjects\\test8.txt");
-    public static File fileReaderTest = new File("C:\\Users\\avanghelovici\\IdeaProjects\\test9.txt");
-    public static File fileWriterTest = new File("C:\\Users\\avanghelovici\\IdeaProjects\\test10.txt");
+    public static File testInputStream = new File("src/TaskStream/sources/test1.txt");
+    public static File testOutputStream = new File("src/TaskStream/sources/test2.txt");
+    public static File testReader = new File("src/TaskStream/sources/test3.txt");
+    public static File testWriter = new File("src/TaskStream/sources/test4.txt");
+    public static File iOreaderTest = new File("src/TaskStream/sources/test5.txt");
+    public static File iOwriterTest = new File("src/TaskStream/sources/test6.txt");
+    public static File bufferedITest = new File("src/TaskStream/sources/test7.txt");
+    public static File bufferedOTest = new File("src/TaskStream/sources/test8.txt");
+    public static File fileReaderTest = new File("src/TaskStream/sources/test9.txt");
+    public static File fileWriterTest = new File("src/TaskStream/sources/test10.txt");
 }
