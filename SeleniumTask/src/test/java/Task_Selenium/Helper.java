7ef37d9540c4f3db3abe0f6fdb21b63427f7383c
avanghelovici <artur.vanghelovici@endava.com>
2019-12-03 21:11:42 +0200

    last commit

--- SeleniumTask/src/test/java/Task_Selenium/Helper.java
+++ SeleniumTask/src/test/java/Task_Selenium/Helper.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 public class Helper {
 
     static final String CREDENTIALS = "artur.test@test.com";
-    static final String DRIVER_LOCATION = "C:\\Users\\avanghelovici\\IdeaProjects\\tryprog\\NewTest\\src\\main\\resources\\chromedriver.exe";
+    static final String DRIVER_LOCATION = "src/main/resources/chromedriver.exe";
 
     static final Wait waiter = new FluentWait<>(driver)
             .withTimeout(10, TimeUnit.SECONDS)
