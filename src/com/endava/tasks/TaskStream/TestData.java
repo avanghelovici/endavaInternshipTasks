06de5a874cee728b69f25d2448ba035263e1b3e6
avanghelovici <artur.vanghelovici@endava.com>
2019-12-21 13:31:12 +0200

    TaskStream moved to tasks package

--- src/com/endava/tasks/TaskStream/TestData.java
+++ src/com/endava/tasks/TaskStream/TestData.java
@@ -1,17 +1,17 @@
-package TaskStream;
+package com.endava.tasks.TaskStream;
 
 
 import java.io.File;
 
 public class TestData {
-    public static File testInputStream = new File("src/TaskStream/sources/test1.txt");
-    public static File testOutputStream = new File("src/TaskStream/sources/test2.txt");
-    public static File testReader = new File("src/TaskStream/sources/test3.txt");
-    public static File testWriter = new File("src/TaskStream/sources/test4.txt");
-    public static File iOreaderTest = new File("src/TaskStream/sources/test5.txt");
-    public static File iOwriterTest = new File("src/TaskStream/sources/test6.txt");
-    public static File bufferedITest = new File("src/TaskStream/sources/test7.txt");
-    public static File bufferedOTest = new File("src/TaskStream/sources/test8.txt");
-    public static File fileReaderTest = new File("src/TaskStream/sources/test9.txt");
-    public static File fileWriterTest = new File("src/TaskStream/sources/test10.txt");
+    public static File testInputStream = new File("src/com.endava.tasks.TaskStream/sources/test1.txt");
+    public static File testOutputStream = new File("src/com.endava.tasks.TaskStream/sources/test2.txt");
+    public static File testReader = new File("src/com.endava.tasks.TaskStream/sources/test3.txt");
+    public static File testWriter = new File("src/com.endava.tasks.TaskStream/sources/test4.txt");
+    public static File iOreaderTest = new File("src/com.endava.tasks.TaskStream/sources/test5.txt");
+    public static File iOwriterTest = new File("src/com.endava.tasks.TaskStream/sources/test6.txt");
+    public static File bufferedITest = new File("src/com.endava.tasks.TaskStream/sources/test7.txt");
+    public static File bufferedOTest = new File("src/com.endava.tasks.TaskStream/sources/test8.txt");
+    public static File fileReaderTest = new File("src/com.endava.tasks.TaskStream/sources/test9.txt");
+    public static File fileWriterTest = new File("src/com.endava.tasks.TaskStream/sources/test10.txt");
 }
